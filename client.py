d74ceb8501c6c871482ff4803bb4156fedb5d669
IpProxyNeon <55587033+IpProxyNeon@users.noreply.github.com>
2020-01-05 13:32:08 +0000

    incomplete

--- client.py
+++ client.py
@@ -4,4 +4,12 @@ api = 'https://discordapp.com/api/v6'
 
 class client(token):
     def send_msg(channel_id, msg:str):
-        
+        headers = {
+          "Authorization":f"Bot {token}",
+          "content": f"{msg}",
+          "tts": false,
+           "embed": {
+            "title": "Hello, Embed!",
+            "description": "This is an embedded message."
+          }
+        }        
