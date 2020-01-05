84585d8a483e98b110bccc42ab866313141eb372
IpProxyNeon <55587033+IpProxyNeon@users.noreply.github.com>
2020-01-05 13:47:38 +0000

    Update client.py

--- client.py
+++ client.py
@@ -2,14 +2,19 @@ import requests
 
 api = 'https://discordapp.com/api/v6'
 
-class client(token):
-    def send_msg(channel_id, msg:str):
+class client(object):
+    def __init__(self, token):
+        self.token = token
+    def send_msg(self, channel_id, msg:str):
+        url = api + f'/channels/{channel_id}/messages'
         headers = {
-          "Authorization":f"Bot {token}",
+          "Authorization":f"Bot {self.token}",
           "content": f"{msg}",
-          "tts": false,
+          "tts": 'false',
            "embed": {
             "title": "Hello, Embed!",
             "description": "This is an embedded message."
-          }
-        }        
+            }
+          }        
+        r = requests.get(url=url, headers=headers)
+        print(r.text)
