08fd36a5407c5617459d20d11ebef9c0935c1069
IpProxyNeon <55587033+IpProxyNeon@users.noreply.github.com>
2020-01-05 13:15:15 +0000

    Update client.py

--- client.py
+++ client.py
@@ -2,10 +2,6 @@ import requests
 
 api = 'https://discordapp.com/api/v6'
 
-class client:
-    def login(token):
-
-    headers = {
-        'Authorization':f'Bot {token}'
-        }
-    requests.get(url=api, headers=headers)
+class client(token):
+    def send_msg(channel_id, msg:str):
+        
